caa89567473498b04fa383f7d8e584fe0fc3ab80
Bernardo Sequeira <bernardosequeir@gmail.com>
2020-12-22 15:26:16 +0000

    day 14 part 1 done!

--- 2020/day14/index.js
+++ 2020/day14/index.js
@@ -1,35 +1,36 @@
 const fs = require('fs')
-const { Z_BEST_COMPRESSION } = require('zlib')
 const inputData = fs.readFileSync('input.txt', 'utf-8').split('\n')
 
 
 const numberToBits = (decimal) => {
-  let bits = Array.from(Array(32)).map(bit => 0)
+  let bits = Array.from(Array(36)).map(bit => 0)
   let number = decimal
-  for (let i = 31; i >= 0; i--) {
-    console.log(2 ** i, number, i);
+  for (let i = 35; i >= 0; i--) {
     if (number >= 2 ** i) {
       number -= 2 ** i
       bits[i] = 1
     }
   }
-  return bits
+
+  return bits.reverse()
 }
 
 const bitsToNumber = (bits) => {
   let number = 0
   for (let i = 0; i < bits.length; i++) {
-    if (bits[i] === 1) number += 2 ** i
+    if (bits[i] === 1) number += 2 ** (35 - i)
   }
   return number
 }
 
 const applyBitmask = (bitmask, bits) => {
+  const newNumber = [...bits]
   for (let i = 0; i < bitmask.length; i++) {
     if (bitmask[i] !== "X") {
-      bits[i] = bitmask[i]
+      newNumber[i] = parseInt(bitmask[i])
     }
   }
+  return newNumber
 }
 
 const parseBitmask = (line) => {
@@ -43,15 +44,26 @@ const parseMemoryAssignment = (line) => {
   return { address, decimal }
 }
 
-console.log(numberToBits(7));
 
-console.log(bitsToNumber([
-  1, 1, 1, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0
-]))
+const parseInstructions = (instrcutionList) => {
+  let mask = []
+  const memory = {}
 
-console.log(parseBitmask('mask = X000X110XX10101X1011100010X100000X00'))
+  instrcutionList.forEach(instruction => {
+    if (instruction.startsWith('mask')) {
+      mask = parseBitmask(instruction)
+    } else {
+      let { address, decimal } = parseMemoryAssignment(instruction)
+      const decimalAsBits = numberToBits(decimal)
+      const bitsAfterBitmask = applyBitmask(mask, decimalAsBits)
+      memory[address] = bitsAfterBitmask
+    }
+  })
 
-console.log(parseMemoryAssignment('mem[57364] = 7578011'));
+  let sum = 0
+  Object.values(memory).forEach(memoryValue => sum += bitsToNumber(memoryValue))
+  console.log(sum);
+}
+
+
+parseInstructions(inputData)
